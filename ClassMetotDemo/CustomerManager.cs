6f9412ab5a8f41cbf247e835b50aa5a26b2439c4
sefik <sefik@DESKTOP-NJI896F>
2023-10-18 13:01:36 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer  customer)
+        {
+            Console.WriteLine("Müşteri Girişi Sağlandı");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Müşteri Bilgileri Güncellendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Müşteri Başarıyla Silindi");
+        }
+        public void GetAll()
+        {
+            Console.WriteLine("Müşteriler Listelendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+
+            customer.Id = 46;
+            customer.FirstName = "Ali";
+            customer.LastName = "Galender";
+
+            CustomerManager customer1 = new CustomerManager();
+            customer1.Add(customer);
+
+
+
+        }
+    }
+}
